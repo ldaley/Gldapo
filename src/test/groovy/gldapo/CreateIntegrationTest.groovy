5626117b24ec9ac759b6f9eb9cf4ac47a67d4469
Luke Daley <ld@ldaley.com>
2008-05-02 07:53:03 +0000

    • Integration test for creating entries.

---
+++ src/test/groovy/gldapo/CreateIntegrationTest.groovy
@@ -0,0 +1,59 @@
+package gldapo
+import gldapo.schema.annotation.GldapoSynonymFor
+
+public class CreateIntegrationTest extends AbstractGldapoIntegrationTest
+{
+    def schemaClasses = [CreateIntegrationTestPerson]
+    
+    void testCreateWithAllAttributes() {
+        def rdn = "cn=allattributes"
+        def p = new CreateIntegrationTestPerson(
+            rdn: rdn,
+            objectClass: ["top", "person", "organizationalPerson"],
+            cn: "Luke",
+            lastName: "surname",
+            telephoneNumber: ["123455", "1245"]
+        )
+        p.save()
+        
+        assertEqualsLdif(rdn, """
+            cn: allattributes
+            sn: surname
+            objectClass: organizationalPerson
+            objectClass: person
+            objectClass: top
+            telephonenumber: 123455
+            telephonenumber: 1245
+        """)
+    }
+    
+    void testCreateWithSomeAttributes() {
+        def rdn = "cn=someattributes"
+        def p = new CreateIntegrationTestPerson(
+            cn: "someattributes",
+            rdn: rdn,
+            objectClass: ["top", "person", "organizationalPerson"],
+            lastName: "sn"
+        )
+        p.save()
+        
+        assertEqualsLdif(rdn, """
+            cn: someattributes
+            sn: sn
+            objectClass: organizationalPerson
+            objectClass: person
+            objectClass: top
+        """)
+    }
+    
+}
+
+
+class CreateIntegrationTestPerson {
+    Set<String> objectClass
+    @GldapoSynonymFor("sn")
+    String lastName
+    String cn
+    String title
+    Set<String> telephoneNumber
+}
