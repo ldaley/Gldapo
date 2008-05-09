119fd5df13480a5dd982164163071cafa9b19d4a
Luke Daley <ld@ldaley.com>
2008-05-09 15:02:35 +0000

    [GLDP-36] - Integration tests for move operations

---
+++ src/test/groovy/gldapo/MoveIntegrationTest.groovy
@@ -0,0 +1,90 @@
+package gldapo
+import gldapo.schema.annotation.GldapoSynonymFor
+
+public class MoveIntegrationTest extends AbstractGldapoIntegrationTest
+{
+    def schemaClasses = [MoveIntegrationTestPerson]
+
+    void testMoveUnmodified() {
+        def p = new MoveIntegrationTestPerson()
+        def cn = "move"
+        def sn = "sn"
+        def rdn = "cn=$cn"
+
+        def constantLdif = """
+            sn: $sn
+            objectClass: person
+            objectClass: top
+        """
+
+        p.rdn = rdn
+        p.objectClass = ["top", "person"]
+        p.cn = cn
+        p.sn = sn
+        p.create()
+
+        assertEqualsLdif(rdn, """
+            cn: $cn
+            $constantLdif
+        """)
+
+        def oldRdn = rdn
+        cn = "moved"
+        rdn = "cn=$cn"
+        p.move(rdn)
+
+        assertNull(getEntry(oldRdn))
+
+        assertEqualsLdif(rdn, """
+            $constantLdif
+            cn: moved
+        """)
+
+    }
+
+    void testMoveModified() {
+        def p = new MoveIntegrationTestPerson()
+        def cn = "move"
+        def sn = "sn"
+        def rdn = "cn=$cn"
+
+        def constantLdif = """
+            objectClass: person
+            objectClass: top
+        """
+
+        p.rdn = rdn
+        p.objectClass = ["top", "person"]
+        p.cn = cn
+        p.sn = sn
+        p.create()
+
+        assertEqualsLdif(rdn, """
+            $constantLdif
+            cn: $cn
+            sn: $sn
+        """)
+
+        def oldRdn = rdn
+        cn = "moved"
+        sn = "sn_changed"
+        rdn = "cn=$cn"
+
+        p.sn = sn
+        p.move(rdn)
+
+        assertNull(getEntry(oldRdn))
+
+        assertEqualsLdif(rdn, """
+            $constantLdif
+            cn: $cn
+            sn: $sn
+        """)
+    }
+}
+
+class MoveIntegrationTestPerson {
+    Set<String> objectClass
+    String sn
+    String cn
+}
