366eff89752b8db9f4f831d69bb46b98e83fc583
Luke Daley <ld@ldaley.com>
2008-10-23 07:53:20 +0000

    [GLDP-97] Preliminary tests
    This should be working, but is failing. It appears to be an apacheds problem, but investigations are continuing.

--- src/test/groovy/gldapo/ByteArrayAttributeIntegrationTest.groovy
+++ src/test/groovy/gldapo/ByteArrayAttributeIntegrationTest.groovy
@@ -20,18 +20,35 @@ public class ByteArrayAttributeIntegrationTest extends AbstractGldapoIntegration
             objectClass: top
             objectClass: inetOrgPerson
             photo:: ${Base64.encode(img1 as byte[])}
+            usercertificate:: ${Base64.encode("4" as byte[])}
+            usercertificate:: ${Base64.encode("5" as byte[])}
+            usercertificate:: ${Base64.encode("6" as byte[])}
         """)
         
         def i = ByteArrayAttributeIntegrationTestPerson.find { eq("cn", cn) }
         assertEquals(img1, i.photo as List)
+        "456".each { y ->
+            assertNotNull(i.usercertificate.find { it == y as Byte[] })
+        }
+        
+        assertTrue("removing cert 1", i.usercertificate.remove(i.usercertificate.find { Arrays.equals(it as byte[], "4" as byte[]) }))
+        assertEquals(2, i.usercertificate.size())
+        
+        i.usercertificate.add("7" as Byte[])
         
         def img2 = []
         this.class.classLoader.findResource("img2.jpg").eachByte { img2 << it }
         i.photo = img2 as byte[]
+        
         i.save()
         
         def i2 = ByteArrayAttributeIntegrationTestPerson.find { eq("cn", cn) }
         assertEquals(img2, i2.photo as List)
+                
+        "567".each { y ->
+            assertNotNull("$y byte not found", i2.usercertificate.find { it == y as Byte[] })
+        }
+        
     }
 }
 
@@ -41,4 +58,5 @@ class ByteArrayAttributeIntegrationTestPerson {
     @GldapoNamingAttribute
     String cn
     byte[] photo
+    Set<Byte[]> usercertificate
 }
